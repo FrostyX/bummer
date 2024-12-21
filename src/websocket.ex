8aa31d61ef20478ce65ac9efe680d4fbc5361cc6
Jakub Kadlcik <frostyx@email.cz>
2024-12-22 00:05:49 +0100

    Allow to configure log level

--- src/websocket.ex
+++ src/websocket.ex
@@ -3,27 +3,31 @@ defmodule Socket do
   use WebSockex
   require Logger
 
+  def set_log_level(level) do
+    Logger.configure(level: level)
+  end
+
   def start_link(url, opts \\ []) do
     WebSockex.start_link(url, __MODULE__, :fake_state, opts)
   end
 
   def echo(client, message) do
-    Logger.info("Sending message: #{message}")
+    Logger.debug("Sending message: #{message}")
     WebSockex.send_frame(client, {:text, message})
   end
 
   def handle_connect(_conn, state) do
-    Logger.info("Connected!")
+    Logger.debug("Connected!")
     {:ok, state}
   end
 
   def handle_frame({:text, msg}, :fake_state) do
-    Logger.info("Received Message: #{msg}")
+    Logger.debug("Received Message: #{msg}")
     {:ok, :fake_state}
   end
 
   def handle_disconnect(%{reason: {:local, reason}}, state) do
-    Logger.info("Local close with reason: #{inspect(reason)}")
+    Logger.debug("Local close with reason: #{inspect(reason)}")
     {:ok, state}
   end
 
--- src/websocket.gleam
+++ src/websocket.gleam
@@ -2,6 +2,9 @@ import gleam/dynamic.{type Dynamic}
 import gleam/erlang/atom.{type Atom}
 import gleam/erlang/process.{type Pid}
 
+@external(erlang, "Elixir.Socket", "set_log_level")
+pub fn set_log_level(level: Atom) -> Nil
+
 @external(erlang, "Elixir.Socket", "start_link")
 pub fn open(url: String) -> Result(Pid, Dynamic)
 
